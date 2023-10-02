8187900611ffc14fa1f04094ce6233cf17d2fbcf
Idil Mahamed <mahamed.idil@gmail.com>
2023-10-01 21:43:43 -0400

    Refactored  Spinner Code

--- spinner2.js
+++ spinner2.js
@@ -1,6 +1,23 @@
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
-setTimeout(() => {
+const cursor = ['\r\\  ','\r| ','\r/  ','\r-  ','\r\\  '];
+for(let i = 0; i < cursor.length; i++) {
+  const spinner = cursor[i];
+  setTimeout(() => {
+    process.stdout.write(spinner)
+
+  }, i * 1000)
+
+};
+
+
+
+
+
+
+
+
+/*setTimeout(() => {
 process.stdout.write('\r| ');
 },100);
 
@@ -15,6 +32,7 @@ setTimeout(() =>{
 setTimeout(() =>{
   process.stdout.write('\r\\  ');
 }, 900);
+
 setTimeout(() => {
   process.stdout.write('\r| ');
   },1100);
@@ -29,4 +47,4 @@ setTimeout(() => {
   
   setTimeout(() =>{
     process.stdout.write('\r\\  ');
-  }, 1700); 
+  }, 1700); */
